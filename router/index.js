d0c2b1f1c71ae6f461804c29a7641dac7d14b07b
rafi <rafie6195@gmail.com>
2023-11-24 19:43:32 +0700

    Penambahan Master Ruangan

--- router/index.js
+++ router/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userRoute } = require("./user.route");
 const { authRoute } = require("./auth.route");
 const { deviceRoute } = require("./device.route");
+const { roomRoute } = require("./room.route");
 
 // const { todoRoute } = require("./todo.route");
 // const { authRoute } = require("./auth.route");
@@ -14,7 +15,7 @@ router.get("/", (req, res) => {
 router.use("/user", userRoute);
 router.use("/auth", authRoute);
 router.use("/device", deviceRoute);
-
+router.use("/room", roomRoute);
 // router.use("/todo", todoRoute);
 // router.use("/auth", authRoute);
 
