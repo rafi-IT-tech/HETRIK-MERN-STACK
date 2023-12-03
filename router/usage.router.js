5bf41450c1bb757eff7d932dd789aa9a267f2764
rafi <rafie6195@gmail.com>
2023-12-03 10:45:47 +0700

    Usage berdasarkan User

--- router/usage.router.js
+++ router/usage.router.js
@@ -11,6 +11,8 @@ router.get('/usage', usageController.getAllUsage);
 // READ - Get usage record by ID
 router.get('/usage/:usageId', usageController.getUsageById);
 
+// New route for getAllByUserId
+router.get('/usage/user/:userID', usageController.getAllByUserId);
 // UPDATE - Update usage record by ID
 router.put('/usage/:usageId', usageController.updateUsageById);
 
